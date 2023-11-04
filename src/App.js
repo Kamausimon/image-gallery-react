5703b1fcbd2729b59bdacbc101b9a8cfc1ae7fdc
kamau simon <kamausimon217@gmail.com>
2023-11-04 18:39:26 +0300

    finalized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import './Assets/tailwind.css'
 import { ImageCard } from "./components/ImageCard";
+import { ImageSearch } from "./components/ImageSearch";
 
 function App() {
 const [images, setImages] = useState([]);
@@ -15,17 +16,20 @@ useEffect(() =>{
     setIsLoading(false);
   })
   .catch(err => console.log(err))
-},[]);
+},[term]);
 
   return (
     <div className=" container mx-auto "> 
-      <div className="grid grid-cols-3 gap-4"> 
+      <ImageSearch searchText ={(text) => setTerm(text)}/>
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images found...</h1> }
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :<div className="grid grid-cols-3 gap-4"> 
       {images.map(image => (
         <ImageCard key={image.id} image={image}/>
       ))}
-      </div>
+      </div>}
     </div>
   );
 }
+
 
 export default App;
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -31,8 +31,10 @@ const tags = image.tags.split(',');
         </ul>
       </div>
       <div className="px-6 py-4">
-      
-
+         {tags.map((tag, index) => 
+             <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 
+       mr-2"> #{tag} </span>
+         )}
       </div>
     </div>
     
